d82f19d63a8046b361927533c2b6b639848b5de7
btamas <tamas@besenyei.net>
2013-09-22 22:08:26 +0200

    new

---
+++ cv.js
@@ -0,0 +1,27 @@
+$(function(){
+	$(".eventTitle").click(function(){
+
+		$(".eventLongDescription")
+			.not(
+				$(this).parent().find(".eventLongDescription")
+			)
+			.animate(
+				{height: "0px"},
+				200,
+				function(){
+					$(this).hide();
+				}
+			);
+
+		var el = $(this).parent().find(".eventLongDescription:hidden"),
+			autoHeight = el.css("height", "auto").height();
+
+		el.height(0).show().animate(
+			{ height: autoHeight },
+			200,
+			function(){
+				$(this).css("height","auto");
+			}
+		);
+	});
+});
