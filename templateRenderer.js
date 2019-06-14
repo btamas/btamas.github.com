be57740259620095b3f9abd5a6fd7c08c28accd4
Tamas Besenyei <tamas@besenyei.net>
2019-06-14 10:34:56 +0200

    refactor

--- templateRenderer.js
+++ templateRenderer.js
@@ -1,49 +1,69 @@
 class TemplateRenderer {
-    constructor(templatePath) {
-        this.templatePath = templatePath;
+    constructor(partialPath) {
+        this.partialPath = partialPath;
     }
 
     async render(name, data) {
         console.log(`render ${name} with ${JSON.stringify(data)}`);
-        if (!document.getElementById(`template-${name}`)) {
+
+        // load template if it has not loaded yet
+        if (!document.getElementById(`partial-${name}`)) {
             await this._loadTemplate(name);
         }
-        const template = document.getElementById(`template-${name}`).content.cloneNode(true);
-        await template.querySelectorAll('template[data-partial]').forEach(async partial => {
-            const partialValueName = partial.getAttribute('data-partial-value');
-            const partialValue = partialValueName ? data[partialValueName] : data;
-            const partialArrayValue = Array.isArray(partialValue) ? partialValue : [partialValue];
 
-            partial.replaceWith(
-                await partialArrayValue.reduce(async (fragment, value) => {
-                    (await fragment).appendChild(await this.render(partial.getAttribute('data-partial'), value));
-                    return fragment;
-                }, Promise.resolve(new DocumentFragment()))
-            );
-        });
-        template.querySelectorAll('[data-value]').forEach(value => {
-            value.innerText = data[value.getAttribute('data-value')];
-            value.removeAttribute('data-value');
-        });
+        const template = document.getElementById(`partial-${name}`).content.cloneNode(true);
 
-        for (let element of template.children) {
-            element.getAttributeNames().forEach(attribute => {
-                if (attribute.startsWith('data-')) {
-                    const attributeName = attribute.substr(5);
-                    const attributeValueName = element.getAttribute(attribute);
-                    element.setAttribute(attributeName, data[attributeValueName]);
-                    element.removeAttribute(attribute);
-                }
-            });
-        }
+        // resolve partials
+        await this._resolvePartials(template, data);
+
+        // resolve content
+        this._resolveContent(template, data);
+
+        // resolve attributes
+        this._resolveAttributes(template, data);
         return template;
     }
 
     async _loadTemplate(name) {
-        const template = document.createElement('template');
-        template.id = `template-${name}`;
-        const response = await fetch(`${this.templatePath}/${name}.html`);
-        template.innerHTML = await response.text();
-        document.head.appendChild(template);
+        const partial = document.createElement('template');
+        partial.id = `partial-${name}`;
+        const response = await fetch(`${this.partialPath}/${name}.html`);
+        partial.innerHTML = await response.text();
+        document.head.appendChild(partial);
+    }
+
+    async _resolvePartials(template, data) {
+        template.querySelectorAll('[data-partial]').forEach(async partial => {
+            const partialContextName = partial.dataset.partialContext;
+            const partialContext = partialContextName ? data[partialContextName] : data;
+
+            partial.replaceWith(
+                await [].concat(partialContext).reduce(async (fragment, value) => {
+                    (await fragment).appendChild(await this.render(partial.dataset.partial, value));
+                    return fragment;
+                }, Promise.resolve(new DocumentFragment()))
+            );
+        });
+    }
+
+    _resolveContent(template, data) {
+        template.querySelectorAll('[data-content]').forEach(value => {
+            value.innerText = data[value.dataset.content];
+            value.removeAttribute('data-content');
+        });
+    }
+
+    _resolveAttributes(template, data) {
+        for (let element of template.children) {
+            element
+                .getAttributeNames()
+                .filter(attribute => attribute.startsWith('data-'))
+                .forEach(attribute => {
+                    const attributeName = attribute.substr(5);
+                    const attributeValueName = element.getAttribute(attribute);
+                    element.setAttribute(attributeName, data[attributeValueName]);
+                    element.removeAttribute(attribute);
+                });
+        }
     }
 }
